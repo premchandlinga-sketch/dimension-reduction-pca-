8dfe86174bdee412314ae7f04d37d268617ff9d1
premchandlinga-sketch <premchandlinga@gmail.com>
2025-12-07 18:40:39 +0530

    Refactor PCA Flask app for improved readability

--- New_PCA_flaskapp.py
+++ New_PCA_flaskapp.py
@@ -82,4 +82,5 @@ def success():
 
 # Running the Flask application
 if __name__ == '__main__':
-    app.run(debug = True, use_reloader=False, port=5001)
+    app.run(debug = True, use_reloader=False)
+
